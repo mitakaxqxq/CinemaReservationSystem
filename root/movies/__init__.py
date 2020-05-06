ceb9747483353bd09e67451dc4da9899d4e1738b
dimitar <mitko.box.hs@gmail.com>
2020-05-06 13:46:00 +0300

    Add initial movies MVC

---
+++ root/movies/__init__.py
@@ -0,0 +1,3 @@
+from .controllers import MovieController
+from .movies_gateway import MovieGateway
+from .models import MovieModel
---
+++ root/movies/movies_queries.py
@@ -0,0 +1,5 @@
+select_all_movies_and_order_by_rating = '''
+SELECT *
+FROM movies
+ORDER BY rating
+'''
