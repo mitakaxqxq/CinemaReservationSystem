fa519244874a90456437b46afdc754e2bf95140b
RalitsaLuka <rali4ka1900@abv.bg>
2020-05-07 21:09:35 +0300

    Modified files

--- root/Reservations/views.py
+++ root/Reservations/views.py
@@ -1,12 +1,22 @@
 import sys
 from .controllers import ReservationContoller
 from .hall import hall
-from . utils import print_hall
-
+from .utils import print_hall
+from root.movies.views import MovieView
 
 class ReservationViews:
     def __init__(self):
         self.controller = ReservationContoller()
+
+
+    def make_reservation(self):
+        print("Let's make your reservation!")
+        tickets = int(input('Input number of tickets you want: '))
+        print('You can choose one of these movies: ')
+        MovieView().get_movies()
+        projection_id = input('Choose projection_id: ')
+        self.show_available_seats(projection_id)
+        self.choose_seats(tickets, projection_id)
 
     def show_available_seats(self, projection_id):
         taken_seats = self.controller.get_rows_and_cols(projection_id)
@@ -17,7 +27,6 @@ class ReservationViews:
                 if create_tuple in taken_seats:
                     current_seats[i][j] = '✖'
         print_hall(current_seats)
-        return current_seats
 
     def find_spots(self, projection_id):
         spots = 0
@@ -27,3 +36,18 @@ class ReservationViews:
                 if seat == '✔':
                     spots = spots + 1
         return spots
+
+    def choose_seats(self, tickets, projection_id):
+        i = 1
+        while i <= tickets:
+            print(f'Choose your seat {i}: ')
+            row = input(f'Choose row: ')
+            col = input(f'Choose column: ')
+            self.check_if_it_is_taken_or_invalid(row, col, projection_id)
+            i = i + 1
+
+    def check_if_it_is_taken_or_invalid(self, row, col, projection_id):
+        taken_seats = self.controller.get_rows_and_cols(projection_id)
+        tup = (row, col)
+        if tup in taken_seats:
+            print('Choose another seat: ')
--- root/users/views.py
+++ root/users/views.py
@@ -1,4 +1,9 @@
 from .controllers import UserContoller
+from root.Projections.views import ProjectionViews
+from root.movies.views import MovieView
+from root.Reservations.views import ReservationViews
+
+import os
 
 
 class UserViews:
@@ -8,11 +13,81 @@ class UserViews:
     def login(self):
         username = input('Username: ')
         password = input('Password: ')
+        result = self.controller.login_user(username=username, password=password)
 
-        return self.controller.login_user(username=username, password=password)
+        self.show_list_of_commands()
 
     def signup(self):
         username = input('Username: ')
         password = input('Password: ')
 
         return self.controller.create_user(username=username, password=password)
+
+    def show_list_of_commands(self):
+        options = f'''
+--------------------
+1. show_movies
+--------------------
+2. show_projections
+--------------------
+3. make_reservation
+--------------------
+4. help
+--------------------
+5. sign_out
+    '''
+        print(options)
+        command = input('>Choose command: ')
+        self.execute_commands(command)
+
+    def execute_commands(self, command):
+        if command == 'help':
+            os.system('clear')
+            self.show_help_command()
+
+        elif command == 'show_projections':
+            os.system('clear')
+            self.show_projections()
+            exit = input('Write exit to return back: ')
+            if exit == 'exit':
+                os.system('clear')
+                self.show_list_of_commands()
+
+        elif command == 'make_reservation':
+            os.system('clear')
+            self.make_reservations()
+
+        elif command == 'show_movies':
+            os.system('clear')
+            self.show_movies()
+            exit = input('Write exit to return back: ')
+            if exit == 'exit':
+                os.system('clear')
+                self.show_list_of_commands()
+        elif command == 'sign_out':
+            print('Sign out')
+
+    def show_help_command(self):
+        show_help = f'''
+---------------------------------------------------------------------------
+|     Show Movies     |     Show Projections     |     Make Resrvation    |
+---------------------------------------------------------------------------
+|Show all current     |   Show all projections   |   Make reservation for |
+|     movies .        |      for all movies.     |     movie you want to  |
+|                     |                          |           watch.       |
+---------------------------------------------------------------------------
+    '''
+        print(show_help)
+        exit = input('Write exit to return back: ')
+        if exit == 'exit':
+            os.system('clear')
+            self.show_list_of_commands()
+
+    def show_movies(self):
+        MovieView().get_movies()
+
+    def show_projections(self):
+        ProjectionViews().show_all_projections()
+
+    def make_reservations(self):
+        ReservationViews().make_reservation()
