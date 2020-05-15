983a8e8fcb27938acfbdd0a105ffe70853b713c0
RalitsaLuka <rali4ka1900@abv.bg>
2020-05-15 15:03:33 +0300

    Modified movies models by orm

--- root/movies/models.py
+++ root/movies/models.py
@@ -1,11 +1,20 @@
 from .validations import validate_movie_id, validate_title, validate_rating
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
+import sys
+sys.path.append('..')
 
 
-class MovieModel:
-    def __init__(self, *, movie_id, title, rating):
-        validate_movie_id(movie_id)
-        validate_title(title)
+from db import Base
+
+
+class Movies(Base):
+    __tablename__ = 'movies'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    rating = Column(Float)
+
+    @staticmethod
+    def validate(name, rating):
+        validate_title(name)
         validate_rating(rating)
-        self.id = movie_id
-        self.title = title
-        self.rating = rating
