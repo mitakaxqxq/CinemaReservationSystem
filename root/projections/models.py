3031ffa6dee595eec05dc44cd3b530d129bc442f
dimitar <mitko.box.hs@gmail.com>
2020-05-15 23:39:12 +0300

    Add ORM projections model

--- root/projections/models.py
+++ root/projections/models.py
@@ -1,14 +1,26 @@
 from .validations import *
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+import sys
+sys.path.append('..')
 
 
-class ProjectionModel:
-    def __init__(self, projection_id, movie_id, projection_type, date_p, time_p):
-        validate_date(date_p)
-        validate_time(time_p)
+from db import Base
+from movies import MovieModel
+
+
+class ProjectionModel(Base):
+    __tablename__ = 'projections'
+    projection_id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer, ForeignKey(MovieModel.movie_id))
+    movie = relationship(MovieModel, backref='projections')
+    projection_type = Column(String)
+    date_p = Column(String)
+    time_p = Column(String)
+
+    @staticmethod
+    def validate_values(projection_id, movie_id, date_p, time_p):
         validate_id(projection_id)
         validate_movie_id(movie_id)
-        self.id = projection_id
-        self.movie_id = movie_id
-        self.type = projection_type
-        self.date_p = date_p
-        self.time_p = time_p
+        validate_date(date_p)
+        validate_time(time_p)
