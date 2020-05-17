cd4305e18f1899393da741b241d89722b0b7e887
dimitar <mitko.box.hs@gmail.com>
2020-05-17 15:18:34 +0300

    Removed unnecessary directory

--- root/db_schema/__init__.py
+++
@@ -1,4 +0,0 @@
-from .users import CREATE_USERS
-from .reservations import CREATE_RESERVATIONS
-from .movies import CREATE_MOVIES
-from .projections import CREATE_PROJECTIONS
--- root/db_schema/users.py
+++
@@ -1,7 +0,0 @@
-CREATE_USERS = '''
-    CREATE TABLE IF NOT EXISTS users(
-        id integer PRIMARY KEY NOT NULL,
-        username varchar(50) UNIQUE NOT NULL,
-        password varchar(50) NOT NULL
-    );
-'''
