26cae003f0895027cbfa1cc1be40451cf2450eca
dimitar <mitko.box.hs@gmail.com>
2020-05-18 11:27:39 +0300

    Add movie gateway tests

--- root/movies/movies_gateway.py
+++ root/movies/movies_gateway.py
@@ -1,52 +1,25 @@
-from root.db import Database
 from .models import MovieModel
-from .movies_queries import *
+from root.db import Base, Session
 
 
 class MovieGateway:
     def __init__(self):
-        self.model = MovieModel
-        self.db = Database()
+        self.db = Base()
+        self.session = Session()
 
     def add_movie(self, title, rating):
-        self.db.create_connection()
-        self.db.connect_cursor()
+        self.session = Session()
+        movie = MovieModel(title=title, rating=rating)
+        self.session.add(movie)
+        self.session.commit()
 
-        query = '''
-        INSERT INTO movies (title, rating)
-        VALUES (?, ?);
-        '''
-        self.db.cursor.execute(query, (title, rating,))
-        self.db.connection.commit()
-        self.db.connection.close()
-
-        self.db.create_connection()
-        self.db.connect_cursor()
-        get_movie_by_id_query = '''
-        SELECT id
-        FROM movies
-        WHERE title = ? and rating = ?;'''
-
-        self.db.cursor.execute(get_movie_by_id_query, (title, rating,))
-        movie_id = self.db.cursor.fetchone()[0]
-
-        self.db.connection.commit()
-        self.db.connection.close()
-        return self.model(movie_id=movie_id, title=title, rating=rating)
+        raw_movie = self.session.query(MovieModel).filter(
+            MovieModel.title == title).filter(MovieModel.rating == rating).one()
+        self.session.close()
+        return raw_movie
 
     def show_movies(self):
-        self.db.create_connection()
-        self.db.connect_cursor()
-
-        self.db.cursor.execute(select_all_movies_and_order_by_rating)
-        raw_movies = self.db.cursor.fetchall()
-
-        self.db.connection.commit()
-        self.db.connection.close()
-        all_movies = []
-
-        for movie in raw_movies:
-            new_movie = self.model(movie_id=movie[0], title=movie[1], rating=movie[2])
-            all_movies.append(new_movie)
-
-        return raw_movies
+        self.session = Session()
+        all_movies = self.session.query(MovieModel).all()
+        self.session.close()
+        return all_movies
